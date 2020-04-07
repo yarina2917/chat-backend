43ed7d93c2f071bbf59b6c8d3f1ac7321b8e41a4
Yana Chorna <yana.chorna@ekreative.com>
2020-04-07 20:55:30 +0300

    fixed last mesagges

--- src/api-routes/messages/messages.js
+++ src/api-routes/messages/messages.js
@@ -6,7 +6,7 @@ const authentication = require('../../services/passport/authentificate-midleware
 const messagesService = require('../../services/messages/messages')
 
 router.get('/messages/:chatId', authentication.apiKey, (req, res, next) => {
-  messagesService.getMessages(req.params.chatId)
+  messagesService.getMessages(req.params.chatId, req.query.lastMessageDate)
     .then(data => res.status(200).send(data))
     .catch(next)
 })
